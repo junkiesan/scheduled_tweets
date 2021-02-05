e62155d049c49c55528b93de8f025792104ce98d
Julien Bouland <bouland.julien@gmail.com>
2021-02-05 18:00:46 +0100

    Accessing the Current User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
+  def current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,6 @@ class PagesController < ApplicationController
 
   def home
     flash[:notice] = "Welcome bro"
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    end
   end
 
   def about
