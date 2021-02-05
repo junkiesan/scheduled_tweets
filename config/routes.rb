29813c3ebd1ffa41a1ea41a8d215c53e06989cde
Julien Bouland <bouland.julien@gmail.com>
2021-02-05 17:33:16 +0100

    Login Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get "about", to: "pages#about"
+  
   get "sign_up", to: "registrations#new"
   post 'sign_up', to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post 'sign_in', to: "sessions#create"
+
   delete 'logout', to "sessions#destroy"
 end
