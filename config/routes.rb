268d380ede57d131f1685eeec833a64cca34ec1b
Julien Bouland <bouland.julien@gmail.com>
2021-02-05 17:13:49 +0100

    Logging Out Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   get "sign_up", to: "registrations#new"
   post 'sign_up', to: "registrations#create"
+  delete 'logout', to "sessions#destroy"
 end
