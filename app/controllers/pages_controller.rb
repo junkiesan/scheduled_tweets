0dc884e4a75f36cefdabf117cc7525f509460ca2
Julien Bouland <bouland.julien@gmail.com>
2021-02-05 16:47:46 +0100

    Login with Session Cookies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
 
   def home
     flash[:notice] = "Welcome bro"
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 
   def about
