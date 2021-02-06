7537d08386248f9cad369c49b2034a4ec7f6a42d
Julien Bouland <bouland.julien@gmail.com>
2021-02-06 19:35:06 +0100

    Password Reset Update

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,36 @@
+class PasswordResetsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user.present?
+      PasswordMailer.with(user: @user).reset.deliver_later
+    end
+    redirect_to root_path, notice: "Email sent with your password"
+  end
+
+  def edit
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: " Your token as ex pired"
+  end
+  
+  def update
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password has been updated."
+    else
+      render :edit
+    end
+  end
+
+  private 
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+end
